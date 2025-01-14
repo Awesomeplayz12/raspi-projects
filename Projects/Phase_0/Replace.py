1f157000c6c9742dce84a26281e240ea9269417f
Abhilash Jai Gudivada <69215192+Awesomeplayz12@users.noreply.github.com>
2025-01-13 21:06:54 -0500

    Create Replace.py

---
+++ Projects/Phase_0/Replace.py
@@ -0,0 +1,7 @@
+# Taking User Input
+user_input = input()
+
+replace_1 = input("What do you want to replace? \n")
+replace_2 = input("What do you want to replace it with? \n")
+
+print(user_input.replace(replace_1, replace_2))
